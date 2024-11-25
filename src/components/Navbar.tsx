51492c110a1f5f7d4df09e0e7218ea9f913cef7b
srajan <kumarsrajan02@gmail.com>
2024-11-25 21:50:36 +0530

    add: mobile-nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { AlignLeft } from "lucide-react";
 
 const Navbar: React.FC = () => {
   return (
@@ -69,6 +78,90 @@ const Navbar: React.FC = () => {
         </ul>
 
         <Button>Connect Wallet</Button>
+      </div>
+      <div className="lg:hidden py-4 px-5 flex justify-between items-center">
+        <NavLink to="/">
+          <div className="logo">
+            <img src="/assets/logo.png" alt="Logo" className="h-14" />
+          </div>
+        </NavLink>
+        <Sheet>
+          <SheetTrigger>
+            <AlignLeft className="w-8 h-8 text-primary" />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>
+                <div className="logo">
+                  <img src="/assets/logo.png" alt="Logo" className="h-14" />
+                </div>
+              </SheetTitle>
+              <SheetDescription></SheetDescription>
+            </SheetHeader>
+            <ul className="flex font-primaryBold tracking-wide flex-col gap-10 py-10 px-2">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary"
+                      : "text-white hover:text-secondary"
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about-us"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary"
+                      : "text-white hover:text-secondary"
+                  }
+                >
+                  About Us
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/roadmap"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary"
+                      : "text-white hover:text-secondary"
+                  }
+                >
+                  Roadmap
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/faqs"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary"
+                      : "text-white hover:text-secondary"
+                  }
+                >
+                  FAQs
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact-us"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-secondary"
+                      : "text-white hover:text-secondary"
+                  }
+                >
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
